74c1a1598e1fa6f99af48462c76bd2dbf481d76d
Siddharth Suresh m1pro mac 2022 <sidd.suresh97@gmail.com>
2022-07-07 14:58:46 -0500

    'fill manually' indicates prompts with errors

--- src/generate_feature_list.py
+++ src/generate_feature_list.py
@@ -88,18 +88,20 @@ def save_feature_triplet_results_in_csv(results_dir, dataset_name, model, exp_na
         concept2_list.append(k[2])
         answer = v[0]['choices'][0]['text'] 
         full_answer_list.append(answer)
-        if k[1] in answer:
+        if k[1] and k[0] in answer:
+            answer_list.append('Fill manually') 
+        elif k[1] in answer:
             answer_list.append(k[1])
         elif k[2] in answer:
             answer_list.append(k[2])
         elif k[0] in answer:
-            answer_list.append(k[0])
+            answer_list.append('fill manually')
             logging.info('{}\n{}\n{}\n{}'.format(k[0], k[1], k[2], v[2], answer))
             print(k[0], k[1], k[2], v[2], answer)
         else:
             logging.info('Unexpected answer') 
             logging.info('{}\n{}\n{}\n{}'.format(k[0], k[1], k[2], v[2], answer))
-            answer_list.append(answer)
+            answer_list.append('fill manually')
             print('Unexpected answer')
             print(k[0], k[1], k[2], v[2], answer) 
             

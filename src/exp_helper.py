a704b9cb8e4575c224264553fbaeedb970296bd5
Siddharth Suresh <sidd.suresh97@gmail.com>
2023-02-02 20:00:42 -0600

    checking if branch changes get commited

--- src/exp_helper.py
+++ src/exp_helper.py
@@ -7,7 +7,10 @@ def run_exp(exp_name, dataset_name, dataset_dir, feature_list_fname, model, open
         df = pd.read_csv(os.path.join(dataset_dir, dataset_name ,feature_list_fname))
         concepts_set, features_set, concept_feature_matrix = create_and_fill_concept_feature_matrix(df)
         estimated_cost(concepts_set, features_set, concept_feature_matrix, exp_name, dataset_name)
-        batches = make_gpt_prompt_batches_feat_listing(concepts_set, features_set, concept_feature_matrix, exp_name)
+        if model != 'flan':
+            batches = make_gpt_prompt_batches_feat_listing(concepts_set, features_set, concept_feature_matrix, exp_name)
+        else:
+            batches = make_flan_prompt_batches_feat_listing(concepts_set, features_set, concept_feature_matrix, exp_name)
     elif exp_name == 'triplet':
         file = open(os.path.join(dataset_dir, dataset_name ,feature_list_fname),'rb')
         triplets = pickle.load(file)
